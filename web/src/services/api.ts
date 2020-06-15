828fb117b874f76830c6f0f6fff2d1a48707bc7e
tsuyan <rafaeldesenhos99@gmail.com>
2020-06-15 17:26:19 +0000

    feature: responsivity in home and create point pages

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333'
+  baseURL: 'https://3333-d9d762b3-0d28-43a7-a121-4b68e84430c6.ws-us02.gitpod.io'
 })
 
 export default api
